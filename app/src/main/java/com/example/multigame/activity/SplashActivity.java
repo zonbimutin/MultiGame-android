d44e528f3b5566043ab7fd7c707298da4a48b8a2
vaudeyb <vaudey.baptiste@gmail.com>
2021-02-08 08:39:11 +0100

    modif splash

--- app/src/main/java/com/example/multigame/activity/SplashActivity.java
+++ app/src/main/java/com/example/multigame/activity/SplashActivity.java
@@ -16,6 +16,11 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+
+
+
+
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
